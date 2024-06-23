e527044d95b7cf414ff58e62d859209b0370bb73
Egor <koropeczegor@gmail.com>
2024-06-23 23:31:12 +0300

    feat: creat assets

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import admin from "./admin.png";
+import appstore from "./appstore.svg";
+import bill from "./bill.png";
+import card from "./card.png";
+import discount from "./discount.svg";
+import person1 from "./person1.png";
+import person2 from "./person2.png";
+import person3 from "./person3.png";
+import playstore from "./playstore.svg";
+import robot from "./robot.png";
+
+export {
+  admin,
+  appstore,
+  bill,
+  card,
+  discount,
+  person1,
+  person2,
+  person3,
+  playstore,
+  robot,
+};
