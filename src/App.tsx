fe8d0081e6d89a835af752d2839850272b460b25
Egor <koropeczegor@gmail.com>
2024-06-24 22:05:26 +0300

    feat: creat stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import NavBar from "./components/Navbar/NavBar";
 import ScrollTop from "./components/ScrollTop/ScrollTop";
+import Hero from "./components/Hero/Hero";
+import Stats from "./components/Stats/Stats";
 
 function App() {
   return (
     <div className="bg-primary text-white w-full h-full">
       <NavBar />
       <ScrollTop />
-      <div className="container px-5 md:px-10 mx-auto"></div>
+      <div className="container px-5 md:px-10 mx-auto">
+        <Hero />
+        <div className="flex flex-col xs:flex-row flex-wrap items-center justify-between gap-6 md:gap-2 py-20">
+          <Stats number="4555" title="Active User" />
+          <Stats number="534" title="Trusted By Company" />
+          <Stats number="$110M" title="Transaction" />
+        </div>
+      </div>
     </div>
   );
 }
