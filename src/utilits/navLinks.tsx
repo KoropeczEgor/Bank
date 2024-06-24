a74e2f3a8a5f4d76458934242c56d62ef3f8c998
Egor <koropeczegor@gmail.com>
2024-06-24 08:50:22 +0300

    feat: create utils nav Links

---
+++ src/utilits/navLinks.tsx
@@ -0,0 +1,22 @@
+interface NavLinks {
+  id: string;
+  title: string;
+}
+
+const navLinks: NavLinks[] = [
+  { id: "home", title: "Home" },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export default navLinks;
